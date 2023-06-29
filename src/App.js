da91138be777f0e9924fb123af677400352c2c9b
Maria1982 <britosmarialuz@gmail.com>
2023-06-29 11:30:52 -0300

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Notes />} />
-          
-       
-        <Route path="/create" element={<Create />} />
-          
-        
-      </Routes>
+      <Switch>
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
     </Router>
   );
 }
