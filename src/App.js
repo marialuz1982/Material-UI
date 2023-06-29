d764c139d57edc6feecd6cac1ee4252a1a35d33a
lbritos <lbritos@tecnoaccion.com.ar>
2023-06-29 11:31:28 -0300

    Cambios

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Notes />} />
           
-       
+        
         <Route path="/create" element={<Create />} />
           
         
